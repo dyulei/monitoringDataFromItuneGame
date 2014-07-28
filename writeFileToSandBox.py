40c7e57b862a38c9d090af4a4df8603489256948
Levi <suiyun@me.com>
2014-07-28 21:03:19 +0800

    Go home !!

--- writeFileToSandBox.py
+++ writeFileToSandBox.py
@@ -1,19 +1,61 @@
 #coding=utf-8
 import urllib
 import re
+import json
+import urllib2
+import sys
 
-def getHtml(url):
-	page = urllib.urlopen(url)
-	html = page.read()
-	return html
+# def getHtml(url):
+# 	page = urllib.urlopen(url)
+# 	html = page.read()
+# 	return html
 
-def writeFile(html):
-	file_object = open('/Users/Levi/github/python/test_web.html', 'w')
-	file_object.write(html)
-	file_object.close()
+# def writeFile(html):
+# 	file_object = open('/Users/Levi/github/python/test_web.html', 'w')
+# 	file_object.write(html)
+# 	file_object.close()
 
-html = getHtml("https://itunes.apple.com/cn/rss/topfreeapplications/limit=10/genre=6014/json")
-writeFile(html)
+# def writeJSONFile(html):
+
+# 	hjson = json.loads(html.read())
+# 	print hjson['feed']['entry'][0]['im:name']['label']
+
+
+
+#html = getHtml("https://itunes.apple.com/cn/rss/topfreeapplications/limit=10/genre=6014/json")
+html = urllib2.urlopen(r'https://itunes.apple.com/cn/rss/topfreeapplications/limit=10/genre=6014/json')
+hjson = json.loads(html.read())
+URL = "https://itunes.apple.com/cn/lookup?id=" + hjson['feed']['entry'][0]['id']['attributes']['im:id']
+print URL
+
+html_URL = urllib2.urlopen(URL)
+hjson1 = json.loads(html_URL.read())
+print hjson1['results'][0]['trackName']
+#writeJSONFile(html)
+
+
+
+# writeFile(html)
+
+# 	x = 0
+# 	for imgurl in imglist:
+# 		urllib.urlretrieve(imgurl, '%s.jpg' % x)
+# 		x+=1
+
+
+# f = file('hello', "w")
+# html = urllib2.urlopen(r'https://itunes.apple.com/cn/rss/topgrossingapplications/limit=10/genre=6014/json')
+
+# hjson = json.loads(html.read())
+
+# print hjson['feed']['entry'][0]['im:name']['label']
+
+
+# f.write(a)
+# print hjson['images']['large']
+# print hjson['summary']
+
+
 
 
 
